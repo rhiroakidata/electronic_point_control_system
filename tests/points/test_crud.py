6c49bf80f41b30db1e3c000eff920e8bd9e13a28
rhiroakidata <rhiroakidata@gmail.com>
2020-06-13 22:50:20 -0300

    Add test report

--- tests/points/test_crud.py
+++ tests/points/test_crud.py
@@ -1,45 +1,45 @@
 import requests, json
 
-# def test_post_point():
-#     headers = {
-#         'Accept': '*/*',
-#         'User-agent': 'request',
-#     }
+def test_post_point():
+    headers = {
+        'Accept': '*/*',
+        'User-agent': 'request',
+    }
     
-#     url = 'http://0.0.0.0:5000/points'
+    url = 'http://0.0.0.0:5000/points'
     
-#     new_point = {
-#         "date": "2020-06-14",
-#         "time": "08:00:00",
-#         "rf": "411703"
-#     }
+    new_point = {
+        "date": "2020-06-14",
+        "time": "08:00:00",
+        "rf": "411703"
+    }
     
-#     response = requests.post(
-#         url,
-#         headers=headers,
-#         data=json.dumps(new_point)
-#     )
-#     response_json = response.json()
-#     status = response.status_code
-#     new_point_response = response_json['data']
+    response = requests.post(
+        url,
+        headers=headers,
+        data=json.dumps(new_point)
+    )
+    response_json = response.json()
+    status = response.status_code
+    new_point_response = response_json['data']
     
-#     assert status == 200 and new_point_response['id'] is not None
+    assert status == 200 and new_point_response['id'] is not None
 
-# def test_get_list_points():
-#     headers = {
-#         'Accept': '*/*',
-#         'User-agent': 'request',
-#     }
+def test_get_list_points():
+    headers = {
+        'Accept': '*/*',
+        'User-agent': 'request',
+    }
     
-#     url = 'http://0.0.0.0:5000/points'
+    url = 'http://0.0.0.0:5000/points'
     
-#     response = requests.get(url, headers=headers)
-#     response_json = response.json()
+    response = requests.get(url, headers=headers)
+    response_json = response.json()
     
-#     status = response_json['status']
-#     list_size = len(response_json['data'])
+    status = response_json['status']
+    list_size = len(response_json['data'])
     
-#     assert status == 200 and list_size > 0
+    assert status == 200 and list_size > 0
     
 def test_get_point():
     headers = {
@@ -84,4 +84,18 @@ def test_delete_point():
     
     assert response.status_code == 200
     
-
+def test_report():
+    headers = {
+        'Accept': '*/*',
+        'User-agent': 'request',
+    }
+    
+    url = 'http://0.0.0.0:5000/collaborators/411703/month/06'
+    
+    response = requests.get(url, headers=headers)
+    response_json = response.json()
+    
+    status = response_json['status']
+    list_size = len(response_json['data'])
+    
+    assert status == 200 and list_size > 0
